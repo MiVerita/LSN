c4ff3c7172d41ef677a50f48c50bf7c10731a44b
MiVerita <michy.veri@gmail.com>
2025-09-04 16:07:24 +0200

    Add files via upload

---
+++ Lezione04/Esercizio_4_2/SOURCE/NSL_SIMULATOR.cpp
@@ -0,0 +1,56 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+
+#include <iostream>
+#include "system.h"
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+
+  int nconf = 1;
+  System SYS;
+  SYS.initialize();
+  SYS.initialize_properties();
+  SYS.block_reset(0);
+
+  for(int i=0; i < SYS.get_nbl(); i++){ //loop over blocks
+    for(int j=0; j < SYS.get_nsteps(); j++){ //loop over steps in a block
+      //Pezzo di codice utile a stampare le velocità istantanea ad un particolare punto della simulazione
+      /*if(i == SYS.get_nbl() - 1 and j == SYS.get_nsteps() - 1){
+        SYS.print_instant_velocity();
+      }*/
+      SYS.step();
+      SYS.measure();
+      //pezzo di codice utile a stampare una particolare configurazione
+      /*if(i == 1 and j == 0){
+        nconf = j+2;
+        cout << nconf <<endl;
+        SYS.write_XYZ(nconf); //Write actual configuration in XYZ format //Commented to avoid "filesystem full"! 
+        //nconf++;
+      }*/
+    }
+    SYS.averages(i+1);
+    SYS.block_reset(i+1);
+  }
+  SYS.finalize();
+
+  return 0;
+}
+
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
